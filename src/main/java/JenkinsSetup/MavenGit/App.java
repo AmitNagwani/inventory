3a83cc70e82a0df516b33fca710d5475c8de6a35
Amit <ameet169@gmail.com>
2018-09-10 13:12:56 +0530

    Testing Phase 2

---
+++ src/main/java/JenkinsSetup/MavenGit/App.java
@@ -0,0 +1,14 @@
+package JenkinsSetup.MavenGit;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
